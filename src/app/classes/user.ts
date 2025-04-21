78140e21e96beb2d67e6cd40a1d97911e5851767
Felipe Devilla <felipedevilla@gmail.com>
2025-04-21 15:26:59 -0300

    Dei continuando as funcionalidades do painel

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,6 +1,8 @@
 export class User {
+    public id!: number;
     public name!: string; 
-    public cpf!: number; 
+    public cpf!: number;
+    public email!: string;
     public password!: string; 
     public isActive!: boolean; 
     public isAdmin!: boolean;
--- src/app/pages/pages-Painel-Adiministrativo/gerenciamento-usuarios/gerenciamento-usuarios.component.ts
+++ src/app/pages/pages-Painel-Adiministrativo/gerenciamento-usuarios/gerenciamento-usuarios.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { User } from '../../../classes/user';
 
 @Component({
   selector: 'app-gerenciamento-usuarios',
-  imports: [],
+  imports: [MatGridListModule],
   templateUrl: './gerenciamento-usuarios.component.html',
   styleUrl: './gerenciamento-usuarios.component.css'
 })
 export class GerenciamentoUsuariosComponent {
+  public usuarios: User[] = [new User()]
 
 }
