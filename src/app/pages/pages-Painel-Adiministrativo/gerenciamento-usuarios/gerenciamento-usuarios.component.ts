b98888bacd30a31ce72557ed96b5b35bb8b4c302
Felipe Devilla <felipedevilla@gmail.com>
2025-04-23 19:50:57 -0300

    Adcionado Icones e botoes nas partes do painel

--- src/app/pages/pages-Painel-Adiministrativo/gerenciamento-usuarios/gerenciamento-usuarios.component.ts
+++ src/app/pages/pages-Painel-Adiministrativo/gerenciamento-usuarios/gerenciamento-usuarios.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 import { User } from '../../../classes/user';
 
 @Component({
   selector: 'app-gerenciamento-usuarios',
-  imports: [MatGridListModule],
+  imports: [MatGridListModule, MatIconModule, MatButtonModule],
   templateUrl: './gerenciamento-usuarios.component.html',
   styleUrl: './gerenciamento-usuarios.component.css'
 })
 export class GerenciamentoUsuariosComponent {
-  public usuarios: User[] = [new User()]
+  public usuarios: User[] = [new User(), new User(), new User()]
 
 }
