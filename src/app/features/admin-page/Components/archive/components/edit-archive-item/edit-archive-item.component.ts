1ff45d4afe87142e47410bd2ee03cf95ef97c59c
Felipe Devilla <felipedevilla@gmail.com>
2025-09-05 06:11:10 -0300

    Finalizado visual do edit

--- src/app/features/admin-page/Components/archive/components/edit-archive-item/edit-archive-item.component.ts
+++ src/app/features/admin-page/Components/archive/components/edit-archive-item/edit-archive-item.component.ts
@@ -3,10 +3,11 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-edit-archive-item',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatCheckboxModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatCheckboxModule, MatButton],
   templateUrl: './edit-archive-item.component.html',
   styleUrl: './edit-archive-item.component.css'
 })
